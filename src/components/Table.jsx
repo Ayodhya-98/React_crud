203cfa084234fec707236b6692ca43cba5194e69
Ayodhya-98 <ayodyanirmanirm@gmail.com>
2023-06-21 12:00:45 +0530

    start  react project

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
+import{BsFillTrashFill,BsFillPencilFill} from "rect-icons/bs"
+
 import "./Table.css";
 
 export const Table=()=>{
-    return<dev>Table</dev>;
+    return<dev>
+        <table>
+            <thead>
+                <tr>
+                    <th>Page</th>
+                    <th>description</th>
+                    <th>Status</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Home</td>
+                    <td>This is the main page</td>
+                    <td>
+                        <span>Live</span>
+                    </td>
+                    <td>
+                        <span>
+                            <BsFillTrashFill/>
+                            <BsFillPencilFill/>
+
+                        </span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </dev>;
 };
