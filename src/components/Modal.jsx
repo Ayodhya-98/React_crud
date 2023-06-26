58d2839fad6023033a75f97b9404816fbf2a6963
Ayodhya-98 <ayodyanirmanirm@gmail.com>
2023-06-26 16:26:09 +0530

    edit  react project

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,20 +6,23 @@ export const Modal=()=>{
     return <div className="modal-container"> 
         <div className="modal">
             <form>
-                <div>
+                <div className="form-group">
                     <label htmlFor="page">Page</label>
                     <input name="page"/>
                 </div>
-                <div>
+                <div className="form-group">
                     <label htmlFor="description">Description</label>
                     <textarea name="description"/>
                 </div>
-                <div>
+                <div className="form-group">
                     <label htmlFor="status">Status</label>
                     <select name="status">
                         <option value="Live">Live</option>
+                        <option value="draft">Draft</option>
+                        <option value="error">Error</option>
                     </select>
                 </div>
+                <button type="submit">Submit</button>
             </form>
         </div>
     </div>;
