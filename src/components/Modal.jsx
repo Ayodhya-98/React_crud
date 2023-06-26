9671772b2852e8d9a1f23a3595d1f2ef3f669db3
Ayodhya-98 <ayodyanirmanirm@gmail.com>
2023-06-26 16:15:19 +0530

    edit  react project

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,6 +4,23 @@ import "./Modal.css";
 
 export const Modal=()=>{
     return <div className="modal-container"> 
-        <div className="modal">Modal</div>
+        <div className="modal">
+            <form>
+                <div>
+                    <label htmlFor="page">Page</label>
+                    <input name="page"/>
+                </div>
+                <div>
+                    <label htmlFor="description">Description</label>
+                    <textarea name="description"/>
+                </div>
+                <div>
+                    <label htmlFor="status">Status</label>
+                    <select name="status">
+                        <option value="Live">Live</option>
+                    </select>
+                </div>
+            </form>
+        </div>
     </div>;
 };
