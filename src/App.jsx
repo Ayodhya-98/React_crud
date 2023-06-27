54f868bedc4ed5240def8c9268aaea26d20076a9
Ayodhya-98 <ayodyanirmanirm@gmail.com>
2023-06-27 14:18:22 +0530

    edit  react project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import {useState} from "react";
+
+
+
 import logo from './logo.svg';
 import './App.css';
 import {Table} from "./components/Table"
 import {Modal} from "./components/Modal"
 
 function App() {
+  const [modalOpen, setModalOpen] =useState(false);
   return (
     <div className="App">
       <Table/>
-      <Modal/>
+      <button className='btn'>Add</button>
+      {modalOpen && <Modal/>}
       
     </div>
   );
