f68a6a024e002d4ba693703dbb176d0acd797036
Ayodhya-98 <ayodyanirmanirm@gmail.com>
2023-07-14 12:22:47 +0530

    edit  react project

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,29 +3,42 @@ import React,{useState} from "react";
 import "./Modal.css";
 
 export const Modal=({closerModal})=>{
-    const[formState,setFormState]=UseState({
+    const[formState,setFormState]=useState({
         page:"",
         description:"",
         status:"live"
     });
+
+    
+
+    const handleChange=(e)=>{
+        setFormState({
+            ...formState,
+            [e.target.name]:e.target.value
+        })
+    }
     return <div className="modal-container" 
     onClick={(e)=>{
         if(e.target.className==="modal-container")
-    closeModal}}
+    closerModal}}
 > 
         <div className="modal" >
             <form>
                 <div className="form-group">
                     <label htmlFor="page">Page</label>
-                    <input name="page"/>
+                    <input name="page" value={formState.page} 
+                    onChange={handleChange}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="description">Description</label>
-                    <textarea name="description"/>
+                    <textarea name="description" value={formState.description} 
+                    onChange={handleChange}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="status">Status</label>
-                    <select name="status">
+                    <select name="status" value={formState.status} 
+                    onChange={handleChange}>
+
                         <option value="Live">Live</option>
                         <option value="draft">Draft</option>
                         <option value="error">Error</option>
